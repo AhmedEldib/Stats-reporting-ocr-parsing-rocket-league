e0a4ba1e6aab40e0ea2e41a8bbfb90597209a53b
AhmedEldib <masterahmed25@hotmial.com>
2020-07-06 01:48:55 +0200

    Fixed an error

--- project_manager.py
+++ project_manager.py
@@ -1,10 +1,11 @@
 from bot_initialization import client
+import functions
 import re
 
 #*********************** rocket stats *************************
 @client.event
 async def on_message(message):
-    if str(message.author.id) != '535120412724690955':
+    if str(message.author.id) != '729481056033570936':
         try:
             img = functions.download_image(message.attachments[0].url)
             team_1, team_2 = functions.get_stats(img)
